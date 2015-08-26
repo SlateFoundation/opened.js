74b1789a3ec17cd10faeb6f0808e05a705adc544
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-08-26 10:30:05 -0400

    add PHP SignedServerRequest class

---
+++ signing_server_examples/php/SignedServerRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OpenEd;
+
+
+class SignedServerRequest
+{
+    private $app_secret;
+    private $client_id;
+
+    public function __construct($app_secret, $client_id)
+    {
+        $this->app_secret = $app_secret;
+        $this->client_id = $client_id;
+    }
+
+    private static function base64UrlEncode($input)
+    {
+        return str_replace('+', '-', str_replace('/', '_', preg_replace('/=+$/', '', base64_encode($input))));
+    }
+
+    private static function generateToken($username)
+    {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            return bin2hex(openssl_random_pseudo_bytes(64));
+        } elseif (function_exists('random_bytes')) {
+            return bin2hex(random_bytes(64));
+        } else {
+            return sha1($username);
+        }
+    }
+
+    public function generateSignedRequest($username)
+    {
+        $envelope = self::base64UrlEncode(json_encode([
+            'username' => $username,
+            'client_id' => $this->client_id,
+            'token' => self::generateToken($username),
+            'algorithm' => 'HMAC-SHA256'
+        ]));
+
+        $signature = self::base64UrlEncode(hash_hmac('SHA256', $envelope, $this->app_secret));
+
+        return "$signature.$envelope";
+    }
+}
